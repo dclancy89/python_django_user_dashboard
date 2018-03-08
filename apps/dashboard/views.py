f0915214e3e2e19d644c52e8df1f23a12c52dcfb
Daniel Clancy <danielclancy@Daniels-MacBook-Pro.local>
2018-03-08 12:48:24 -0600

    added login page

--- apps/dashboard/views.py
+++ apps/dashboard/views.py
@@ -3,3 +3,6 @@ from django.shortcuts import render
 # Create your views here.
 def index(request):
 	return render(request, 'dashboard/index.html')
+
+def signin(request):
+	return render(request, 'dashboard/signin.html')
